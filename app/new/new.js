5ba4fb2ab16210c3be58a7cff2b7d5088dc3b2ef
henriyli <henri.ylikotila@helsinki.fi>
2015-08-26 13:23:12 +0300

    adding a custom validation directive for the multiselect

--- app/new/new.js
+++ app/new/new.js
@@ -38,4 +38,22 @@ angular.module('myApp.new', ['ngRoute'])
     }
     return stars;
   };
-}]);
+}])
+
+.directive('uiSelectRequired', function () { 
+  return { require: 'ngModel', link: function (scope, elm, attrs, ctrl) { 
+    ctrl.$validators.uiSelectRequired = function (modelValue, viewValue) {
+            var determineVal;
+            if (angular.isArray(modelValue)) {
+                determineVal = modelValue;
+            } else if (angular.isArray(viewValue)) {
+                determineVal = viewValue;
+            } else {
+                return false;
+            }
+
+            return determineVal.length > 0;
+        };
+    }
+  };
+}); 
