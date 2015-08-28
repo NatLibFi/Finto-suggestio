e07a26349bbd38dcd31a7c4ce48eceeecd71d661
henriyli <henri.ylikotila@helsinki.fi>
2015-08-28 13:24:07 +0300

    adding human readable labels for the submission property headers

--- app/change/change.js
+++ app/change/change.js
@@ -14,20 +14,22 @@ app.controller('ChangeController', ['$scope', function($scope) {
   $scope.requestFormatter = function(qstring) {
     return {query: qstring + '*'};
   };
+  this.labels = {'type': 'Muutos', 'preflabel': 'Päätermi/asiasana', 'state': 'Tila', 'change':'Ehdotettu muutos', 'explanation': 'Perustelut ehdotukselle', 'fromname': 'Ehdottajan nimi', 'fromemail': 'Ehdottajan sähköpostiosoite'};
 
-  this.suggestion = {type: 'Muutos', preflabel: '', state: 'Käsittelyssä', date: Date.now(), comments: 0};
+  this.suggestion = {type: 'Muutos', preflabel: '', state: 'Käsittelyssä'};
 
   this.submitSuggestion = function() {
     var msg_body = '';
     for (var property in this.suggestion) {
+      var proplabel = this.labels[property] ? this.labels[property] : property;
       var propval = this.suggestion[property];
       if (propval.originalObject) // if there is an uri available from the autocomplete linking to that
-        msg_body += '### ' + property + '   \n' + '[' + propval.originalObject.prefLabel + '](' + propval.originalObject.uri + ')  \n';
+        msg_body += '### ' + proplabel + '   \n' + '[' + propval.originalObject.prefLabel + '](' + propval.originalObject.uri + ')  \n';
       else // the property value is only a string
-        msg_body += '### ' + property + '   \n' + propval + '  \n';
+        msg_body += '### ' + proplabel + '   \n' + propval + '  \n';
     }
     console.log(msg_body);
-    this.suggestion = {type: 'Muutos', preflabel: '', state: 'Käsittelyssä', date: Date.now(), comments: 0};
+    this.suggestion = {type: 'Muutos', preflabel: '', state: 'Käsittelyssä'};
   };
 }]);
 
