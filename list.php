637554c41779782f666fab718d985eab39324d01
henriyli <henri.ylikotila@helsinki.fi>
2015-09-07 16:02:45 +0300

    simple php scripts for interacting with the gh api

---
+++ list.php
@@ -0,0 +1,13 @@
+<?php
+header("Content-type: application/json");
+
+require_once('.token');
+
+$url = 'https://api.github.com/repos/henriyli/issuetest/issues?state=open';
+$session = curl_init($url);
+curl_setopt($session, CURLOPT_USERPWD, $suggestio_token);
+curl_setopt($session, CURLOPT_USERAGENT, "https://github.com/NatLibFi/Finto-suggestio");
+curl_setopt($session, CURLOPT_HTTPHEADER, array("Content-type: application/json", "Accept: application/vnd.github.v3+json"));
+$response = curl_exec($session);
+curl_close($session);
+json_encode($response);
