852be33b1bc3261f43a4a1f1198defcda9799171
henriyli <henri.ylikotila@helsinki.fi>
2016-08-22 15:16:54 +0300

    fixing the broken css ref

--- gulpfile.js
+++ gulpfile.js
@@ -10,7 +10,7 @@ gulp.task('default', function() {
       .pipe(concat('app/dist.js'))
       .pipe(gulp.dest('.'))
     
-    gulp.src(['app/bower_components/bootstrap/dist/css/bootstrap.min.css', 'app/bower_components/angular-ui-select/dist/css/select.min.css', 'app/bower_components/angucomplete-alt/angucomplete-alt.css'])
+    gulp.src(['app/bower_components/bootstrap/dist/css/bootstrap.min.css', 'app/bower_components/angular-ui-select/dist/select.min.css', 'app/bower_components/angucomplete-alt/angucomplete-alt.css'])
       .pipe(concat('app/dist.css'))
       .pipe(gulp.dest('.'))
 });
@@ -18,5 +18,11 @@ gulp.task('default', function() {
 gulp.task('js', function() {
     gulp.src(['app/app.js', 'app/new/new.js', 'app/change/change.js', 'app/list/list.js'])
       .pipe(concat('app/dist.js'))
+      .pipe(gulp.dest('.'))
+});
+
+gulp.task('css', function() {
+    gulp.src(['app/bower_components/bootstrap/dist/css/bootstrap.min.css', 'app/bower_components/angular-ui-select/dist/select.min.css', 'app/bower_components/angucomplete-alt/angucomplete-alt.css'])
+      .pipe(concat('app/dist.css'))
       .pipe(gulp.dest('.'))
 });
