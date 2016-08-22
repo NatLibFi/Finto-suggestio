01324d478583d1be98833e90112093f842039932
henriyli <henri.ylikotila@helsinki.fi>
2016-08-22 12:26:21 +0300

    concatting css too

--- gulpfile.js
+++ gulpfile.js
@@ -9,4 +9,8 @@ gulp.task('default', function() {
     gulp.src(['app/app.js', 'app/new/new.js', 'app/change/change.js', 'app/list/list.js'])
       .pipe(concat('app/dist.js'))
       .pipe(gulp.dest('.'))
+    
+    gulp.src(['app/bower_components/bootstrap/dist/css/bootstrap.min.css', 'app/bower_components/angular-ui-select/dist/css/select.min.css', 'app/bower_components/angucomplete-alt/angucomplete-alt.css'])
+      .pipe(concat('app/dist.css'))
+      .pipe(gulp.dest('.'))
 });
