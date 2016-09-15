4597befc5ed495d42834a1cb29030fff210b820e
henriyli <henri.ylikotila@helsinki.fi>
2016-09-15 16:59:03 +0300

    rewriting the gulpfile and adding a watch task for convenience

--- gulpfile.js
+++ gulpfile.js
@@ -1,22 +1,21 @@
 var gulp = require('gulp');
 var concat = require('gulp-concat');
+var paths = {
+  code: ['app/app.js', 'app/new/new.js', 'app/change/change.js', 'app/list/list.js']
+};
 
-gulp.task('default', function() {
+gulp.task('deps', function() {
     gulp.src(['app/bower_components/**/*.min.js'])
       .pipe(concat('app/dist_components.js'))
       .pipe(gulp.dest('.'))
 
-    gulp.src(['app/app.js', 'app/new/new.js', 'app/change/change.js', 'app/list/list.js'])
-      .pipe(concat('app/dist.js'))
-      .pipe(gulp.dest('.'))
-    
     gulp.src(['app/bower_components/bootstrap/dist/css/bootstrap.min.css', 'app/bower_components/angular-ui-select/dist/select.min.css', 'app/bower_components/angucomplete-alt/angucomplete-alt.css'])
       .pipe(concat('app/dist.css'))
       .pipe(gulp.dest('.'))
 });
 
 gulp.task('js', function() {
-    gulp.src(['app/app.js', 'app/new/new.js', 'app/change/change.js', 'app/list/list.js'])
+    gulp.src(paths.code)
       .pipe(concat('app/dist.js'))
       .pipe(gulp.dest('.'))
 });
@@ -26,3 +25,9 @@ gulp.task('css', function() {
       .pipe(concat('app/dist.css'))
       .pipe(gulp.dest('.'))
 });
+
+gulp.task('watch', function() { 
+    gulp.watch(paths.code, ['js']);
+});
+
+gulp.task('default', ['watch', 'js']);
